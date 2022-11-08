8e242e2ccfcd966a6025765e132de0d71bdbffdd
sunafterrainwm <sunafterrainwm@gmail.com>
2022-11-08 22:19:26 +0800

    =

--- src/util.ts
+++ src/util.ts
@@ -9,3 +9,30 @@ export function pick<O extends object, K extends keyof O>(object: O, keys: K[]):
 
 	return result as Pick<O, K>;
 }
+
+export enum HtmlSpecialCharsOption {
+	/* Will convert double-quotes and leave single-quotes alone. */
+	Compat,
+	/* Will convert both double and single quotes. */
+	Quotes,
+	/* Will leave both double and single quotes unconverted. */
+	NoQuotes
+}
+
+export function htmlSpecialChars(text: string, option: HtmlSpecialCharsOption): string {
+	const replaceTable: Record<string, string> = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;'
+	};
+
+	if (option === HtmlSpecialCharsOption.Quotes) {
+		replaceTable['\''] = '&apos;';
+	} else if (option === HtmlSpecialCharsOption.NoQuotes) {
+		delete replaceTable['"'];
+	}
+	return text.replace(/[&<>'"]/g, function (char) {
+		return replaceTable[char] ?? char;
+	});
+}
